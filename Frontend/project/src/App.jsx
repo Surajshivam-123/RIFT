1acc7b44ca3e8657c3ea8badf247acb43917ecbe
Surajshivam-123 <sj919810@gmail.com>
2026-02-18 23:25:53 +0530

    start-to-build

---
+++ Frontend/project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">🚀 Hackathon 2026</h1>
+      <p className="text-gray-600">Build • Innovate • Win</p>
+      <button className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">Register</button>
+    </div>
+    </>
+  )
+}
+
+export default App
